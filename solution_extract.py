e966a8d3544ae374bc3309bd91883575bb6143a1
SpencerGoldrich <73447816+SpencerGoldrich@users.noreply.github.com>
2022-12-03 18:54:47 -0500

    Update solution_extract.py

--- solution_extract.py
+++ solution_extract.py
@@ -6,55 +6,48 @@ import pybamm
 import matplotlib.pyplot as plt
 import numpy as np
 
+#Can Specify cutoff voltage
+Experiment = pybamm.Experiment(
+    [
+        ('Discharge at 17A until 3.0V')
+    ]
+)
+
 #These are base parameters from the paper and our research
 parameters = pybamm.ParameterValues("Ramadass2004")
-parameters["Current function [A]"] = 2.138 #Our dim 1.4V for 1C, 0.28V for 0.2C, Their dim 2.138A for 1C and 0.4267A for 0.2C
-#parameters["Electrode height [m]"] = 0.2
-#parameters["Electrode width [m]"] = 0.2
-parameters["Lower voltage cut-off [V]"] = 3
+parameters["Electrode height [m]"] = 0.2
+parameters["Electrode width [m]"] = 0.2
 parameters["Negative electrode density [kg.m-3]"] = 2199
 parameters["Negative particle radius [m]"] = 2.1e-05
-parameters["Nominal cell capacity [A.h]"] = 2.138 #Our dim 1.4, Their dim 2.138
-#parameters["Number of cells connected in series to make a battery"] = 15
-#parameters["Number of electrodes connected in parallel to make a cell"] = 71
 parameters["Positive electrode density [kg.m-3]"] = 4239
 parameters["Separator density [kg.m-3]"] = 916.8
 parameters["Separator porosity"] = 0.39
-
+#parameters["Number of cells connected in series to make a battery"] = 15
+#parameters["Number of electrodes connected in parallel to make a cell"] = 71
 
 #Test parameters from Cara's Excel work
 testparams = {
     "porosity":[0.1,0.2,0.3,0.4,0.5,0.6,0.7],
-    "athick":np.multiply((10**(-6)),[67.40559574, 75.83129521, 86.66433738, 101.1083936, 121.3300723, 151.6625904, 202.2167872]),
-    "aactivevolfrac":[0.7762910539, 0.6900364923, 0.6037819308, 0.5175273692, 0.4312728077, 0.3450182462, 0.2587636846],
     "cthick":np.multiply((10**(-6)),[63.29051709, 71.20183172, 81.37352197, 94.93577563, 113.9229308, 142.4036634, 189.8715513]),
-    "cactivevolfrac":[0.6636728906, 0.5899314583, 0.516190026, 0.4424485937, 0.3687071614, 0.2949657291, 0.2212242969]
 }
-'''testparams = {
-    "porosity":[0.4],
-    "athick":np.multiply((10**(-6)),[101.1083936]),
-    "aactivevolfrac":[0.5175273692],
-    "cthick":np.multiply((10**(-6)),[94.93577563]),
-    "cactivevolfrac":[0.4424485937]
-}'''
-
-#Can Specify cutoff voltage
-Experiment = pybamm.Experiment(
-    [
-        ('Discharge at 1C until 3.0V')
-    ]
-)
+#These below are base cases
+parameters["Positive electrode porosity"] = testparams["porosity"][2]
+parameters["Positive electrode thickness [m]"] = testparams["cthick"][2]
 
 #Anode testing
-def test(Experiment, parameters, testparams):
+def test(Experiment, parameters, testparams, ps):
     cap = []
-    for i in range(0,len(testparams['porosity'])):
-        parameters["Negative electrode porosity"] = testparams['porosity'][i]
-        parameters["Negative electrode thickness [m]"] = testparams['athick'][i]
-        parameters["Negative electrode active material volume fraction"] = testparams['aactivevolfrac'][i]
+    time = []
+    #Len is 7 so 0:6 is max range
+    for i in range(ps[0],ps[1]):
         parameters["Positive electrode porosity"] = testparams['porosity'][i]
-        parameters["Positive electrode thickness [m]"] = testparams['cthick'][i]
-        parameters["Positive electrode active material volume fraction"] = testparams['cactivevolfrac'][i]
+        #parameters["Positive electrode thickness [m]"] = testparams['cthick'][i]
+        parameters["Positive electrode thickness [m]"] = 1000*(10**(-6))
+
+        parameters["Positive electrode active material volume fraction"] = (1-parameters["Positive electrode porosity"])/0.737
+        parameters["Negative electrode porosity"] = parameters["Positive electrode porosity"]
+        parameters["Negative electrode thickness [m]"] = parameters["Positive electrode thickness [m]"]/0.939
+        parameters["Negative electrode active material volume fraction"] = parameters["Positive electrode active material volume fraction"]/0.855
 
         sim = pybamm.Simulation(pybamm.lithium_ion.DFN(), experiment=Experiment, parameter_values=parameters)
         sim._solver.return_solution_if_failed_early = True
@@ -62,15 +55,12 @@ def test(Experiment, parameters, testparams):
         sol = sim.solution
 
         cap.append(sol._summary_variables['Capacity [A.h]'])
+        time.append(sol._summary_variables['Time [h]'])
 
     finalsim = sim
-    return(finalsim, cap)
+    return(finalsim, cap, time)
 
-finalsim, cap = test(Experiment,parameters,testparams)
+finalsim, cap, time = test(Experiment,parameters,testparams,[4,5])
 sol = finalsim.solution
-print(sol._summary_variables['Capacity [A.h]'])
-print(sol._summary_variables['Positive electrode capacity [A.h]'])
-print(sol._summary_variables['Negative electrode capacity [A.h]'])
-#finalsim.plot()
 
 plt.plot(range(0,7),cap)
