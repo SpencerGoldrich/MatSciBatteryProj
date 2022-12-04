e5f7a325a35c926c8da58f5565da6a9ccb783e6d
SpencerGoldrich <73447816+SpencerGoldrich@users.noreply.github.com>
2022-12-03 19:49:04 -0500

    Update solution_extract.py

--- solution_extract.py
+++ solution_extract.py
@@ -9,7 +9,7 @@ import numpy as np
 #Can Specify cutoff voltage
 Experiment = pybamm.Experiment(
     [
-        ('Discharge at 17A until 3.0V')
+        ('Discharge at 1A until 3.0V')
     ]
 )
 
@@ -19,30 +19,33 @@ parameters["Electrode height [m]"] = 0.2
 parameters["Electrode width [m]"] = 0.2
 parameters["Negative electrode density [kg.m-3]"] = 2199
 parameters["Negative particle radius [m]"] = 2.1e-05
+parameters["Positive particle radius [m]"] = 10e-6
 parameters["Positive electrode density [kg.m-3]"] = 4239
 parameters["Separator density [kg.m-3]"] = 916.8
 parameters["Separator porosity"] = 0.39
-#parameters["Number of cells connected in series to make a battery"] = 15
-#parameters["Number of electrodes connected in parallel to make a cell"] = 71
+parameters["Number of cells connected in series to make a battery"] = 15
+parameters["Number of electrodes connected in parallel to make a cell"] = 71
 
 #Test parameters from Cara's Excel work
 testparams = {
     "porosity":[0.1,0.2,0.3,0.4,0.5,0.6,0.7],
-    "cthick":np.multiply((10**(-6)),[63.29051709, 71.20183172, 81.37352197, 94.93577563, 113.9229308, 142.4036634, 189.8715513]),
+    "cthick":np.multiply((10**(-6)),range(20,900,10)),
 }
 #These below are base cases
-parameters["Positive electrode porosity"] = testparams["porosity"][2]
-parameters["Positive electrode thickness [m]"] = testparams["cthick"][2]
+parameters["Positive electrode porosity"] = 0.3
+parameters["Positive electrode thickness [m]"] = 80*10**(-6)
 
 #Anode testing
 def test(Experiment, parameters, testparams, ps):
     cap = []
     time = []
-    #Len is 7 so 0:6 is max range
+    j = 0
     for i in range(ps[0],ps[1]):
-        parameters["Positive electrode porosity"] = testparams['porosity'][i]
+        j += 1
+        print(j)
+
+        #parameters["Positive electrode porosity"] = testparams['porosity'][i]
         #parameters["Positive electrode thickness [m]"] = testparams['cthick'][i]
-        parameters["Positive electrode thickness [m]"] = 1000*(10**(-6))
 
         parameters["Positive electrode active material volume fraction"] = (1-parameters["Positive electrode porosity"])/0.737
         parameters["Negative electrode porosity"] = parameters["Positive electrode porosity"]
@@ -60,7 +63,7 @@ def test(Experiment, parameters, testparams, ps):
     finalsim = sim
     return(finalsim, cap, time)
 
-finalsim, cap, time = test(Experiment,parameters,testparams,[4,5])
-sol = finalsim.solution
+sim, cap, time = test(Experiment,parameters,testparams,[0,1])
+sol = sim.solution
 
 plt.plot(range(0,7),cap)
